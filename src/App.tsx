cc8aea703c0c21809d3f60a9ec00e314935c1746
Manvendra Singh <manvendrask2@gmail.com>
2025-04-15 14:10:50 +0530

    feat: panda css integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { css } from '../styled-system/css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      className={css({
+        bg: 'red.400',
+        '&:hover': {
+          bg: 'orange.400',
+        },
+      })}
+    >
+      <div
+        className={css({
+          display: 'grid',
+          flexDirection: 'column',
+          gap: '4',
+          alignItems: 'center',
+        })}
+      >
+        <h1 className={css({ fontSize: '2xl', fontWeight: 'bold' })}>
+          Change branches to see the individual projects
+        </h1>
+        <h1 className={css({ fontSize: '2xl', fontWeight: 'bold' })}>
+          Change branches to see the individual projects
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
